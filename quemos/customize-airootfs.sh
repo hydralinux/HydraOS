9b4993e08cd13b3ec4c73eca685960b56b2244a5
Muhammed-Mert <75245988+Muhammed-Mert@users.noreply.github.com>
2023-04-20 20:25:21 +0300

    Update customize-airootfs.sh

--- quemos/customize-airootfs.sh
+++ quemos/customize-airootfs.sh
@@ -6,7 +6,6 @@ apt install wget
 wget https://github.com/quemos/deb/releases/download/deb/17g-installer_1.0_all.deb
 apt install ./*.deb -yq --allow-downgrades
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-##nano usr/share/applications/gdebi.desktop ##exec--> sh -c "gdebi-gtk %f"
 
 #### Disable recommends by default
 cat > /etc/apt/apt.conf.d/01norecommend << EOF
